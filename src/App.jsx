2646b260aeb793ffa5b9eb338f65a87e366e771f
marcladefoged <marcladefogedjensen@gmail.com>
2024-06-08 03:45:01 +0200

    Features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
@@ -7,7 +8,8 @@ const App = () => {
     <>
     <Navbar />
     <div className="max-w-7xl mx-auto pt-20 px-6">
-    <HeroSection />
+      <HeroSection />
+      <FeatureSection />
     </div>
     </>
   )
