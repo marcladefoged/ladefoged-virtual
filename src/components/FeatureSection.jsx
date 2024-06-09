f229eeccc95ed2436c00d5ef814e889fcceefa92
marcladefoged <marcladefogedjensen@gmail.com>
2024-06-10 01:59:26 +0200

    last commit

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,7 +1,5 @@
 import { features } from "../constants";
 
-// https://www.youtube.com/watch?v=EwzWg-Joxq0
-
 const FeatureSection = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
